75a2529a71ef1e45575b0c852c4682dfc33c4f59
hamzaguyo725 <hamzaguyo725@gmail.com>
2023-05-06 16:41:24 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+import React, { useState} from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
-  const [page, setPage] = useState("List");
+  const [currentPage, setCurrentPage] = useState("List");
 
   return (
     <main>
-      <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      <AdminNavBar PageChange={setCurrentPage} />
+      {currentPage === "Form" ? <QuestionForm /> : <QuestionList />}
     </main>
   );
 }
